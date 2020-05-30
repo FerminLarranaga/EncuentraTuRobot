ca9379d51f3e37d29cf843ecd76cd777c4d78afb
FerminLarranaga <64256655+FerminLarranaga@users.noreply.github.com>
2020-05-30 20:56:05 -0300

    adding files

---
+++ precache-manifest.028acfbea6473999a11e9e9d3686ea63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5264929f2332a35bcf3eaa73800c2fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241671f6f3ea7830dd59",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "42bda54ba9aac59485e6",
+    "url": "/static/css/main.e7d7a4a8.chunk.css"
+  },
+  {
+    "revision": "241671f6f3ea7830dd59",
+    "url": "/static/js/2.229bacee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.229bacee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bda54ba9aac59485e6",
+    "url": "/static/js/main.04e727e7.chunk.js"
+  },
+  {
+    "revision": "321e04a6baf10d2dc5fb",
+    "url": "/static/js/runtime-main.8fdaba61.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
